2f60755611736af5378adae1d31e78d18f2d85c7
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-08-13 19:53:24 -0300

    chore: update dependencies and add ESLint configuration
    - Added ESLint and TypeScript Node as dev dependencies.
    - Updated package.json to include @rocketseat/eslint-config.
    - Refactored server.ts to use single quotes for strings for consistency.

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,26 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const server = fastify()
 
 const courses = [
   {
     id: 1,
-    name: "Node.js Basics",
+    name: 'Node.js Basics',
   },
   {
     id: 2,
-    name: "Advanced JavaScript",
+    name: 'Advanced JavaScript',
   },
   {
     id: 3,
-    name: "Web Development with React",
+    name: 'Web Development with React',
   },
 ]
 
-server.get("/courses", async (request, reply) => {
+server.get('/courses', async (request, reply) => {
   return courses
 })
 
 server.listen({ port: 3333 }).then(() => {
-  console.log("Server is running on http://localhost:3333")
+  console.log('Server is running on http://localhost:3333')
 })
