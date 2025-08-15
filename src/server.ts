024bb3e6001da185d3a4f4b01089f0aaea63c5d8
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-08-14 23:04:01 -0300

    feat: integrate drizzle ORM for database management
    - Updated package.json to include drizzle-orm and pg dependencies.
    - Refactored server.ts to implement database interactions for courses.
    - Added CRUD endpoints for courses with database integration.
    - Created drizzle configuration file for PostgreSQL connection.
    - Added Docker Compose configuration for PostgreSQL service.
    - Defined database schema for users and courses in schema.ts.
    - Generated initial migration files for users and courses tables.
    - Updated tsconfig.json to allow importing TypeScript extensions.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import fastify from 'fastify'
+import { eq } from 'drizzle-orm'
+
+import { db } from './database/client.ts'
+import { courses } from './database/schema.ts'
 
 const server = fastify({
   logger: {
@@ -12,23 +16,58 @@ const server = fastify({
   },
 })
 
-const courses = [
-  {
-    id: 1,
-    name: 'Node.js Basics',
-  },
-  {
-    id: 2,
-    name: 'Advanced JavaScript',
-  },
-  {
-    id: 3,
-    name: 'Web Development with React',
-  },
-]
-
 server.get('/courses', async (request, reply) => {
-  return courses
+  const result = await db
+    .select({
+      id: courses.id,
+      title: courses.title,
+    })
+    .from(courses)
+
+  return reply.send({ courses: result })
+})
+
+server.get('/courses/:id', async (request, reply) => {
+  type Params = {
+    id: string
+  }
+
+  const params = request.params as Params
+  const courseId = params.id
+
+  const result = await db.select().from(courses).where(eq(courses.id, courseId))
+
+  if (result.length > 0) {
+    return { course: result[0] }
+  }
+
+  return reply.status(404).send()
+})
+
+server.post('/courses', async (request, reply) => {
+  type Body = {
+    title: string
+    description: string
+  }
+
+  const courseId = crypto.randomUUID()
+
+  const body = request.body as Body
+  const courseTitle = body.title
+
+  if (!courseTitle) {
+    return reply.status(400).send({ message: 'Título é obrigatório!' })
+  }
+
+  const result = await db
+    .insert(courses)
+    .values({
+      title: courseTitle,
+      description: body.description,
+    })
+    .returning()
+
+  return reply.status(201).send({ courseId: result[0].id })
 })
 
 server.listen({ port: 3333 }).then(() => {
