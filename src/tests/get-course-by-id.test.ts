09eb53005f07b1d262dbe1b98f163e022bb84640
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-09-13 11:11:46 -0300

    feat: update package.json to include coverage-v8 and modify test script for coverage reporting

--- src/tests/get-course-by-id.test.ts
+++ src/tests/get-course-by-id.test.ts
@@ -20,3 +20,13 @@ test('get a course by id', async () => {
     },
   })
 })
+
+test('return 404 status for non existing courses', async () => {
+  await server.ready()
+
+  const response = await request(server.server).get(
+    '/courses/dd7f75a4-0c98-47ca-b497-57dd028b473b',
+  )
+
+  expect(response.status).toBe(404)
+})
